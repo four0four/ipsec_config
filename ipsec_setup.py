5c74c7df4d7e48c2a973aa0a7c06fbab6e00a244
Galen <Shmeeeh@gmail.com>
2012-07-18 17:53:13 -0400

    fixes. forgot write/create mode on file objects.

--- ipsec_setup.py
+++ ipsec_setup.py
@@ -449,9 +449,9 @@ def main():
 
 if __name__ == '__main__':
 	#global crap - hackishly using tmpfiles for now, because python can't pass by reference like a sane language
-	config_setup = open('/tmp/config_setup')
-	conn_default = open('/tmp/conn_default')
-	conn_subnet_extrusion = open('/tmp/connf_subnet_extrusion')
+	config_setup = open('/tmp/config_setup','w')
+	conn_default = open('/tmp/conn_default','w')
+	conn_subnet_extrusion = open('/tmp/connf_subnet_extrusion','w')
 	
 	config_setup.write("config setup\n")
 	conn_default.write("conn %default\n")
